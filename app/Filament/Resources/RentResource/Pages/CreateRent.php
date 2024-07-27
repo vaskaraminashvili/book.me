11334513f1e378710ec82c09dfcf3f282b4433bb
Vasili Raminashvili <vaska.r02@gmail.com>
2024-07-28 02:38:34 +0400

    range added

--- app/Filament/Resources/RentResource/Pages/CreateRent.php
+++ app/Filament/Resources/RentResource/Pages/CreateRent.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Resources\RentResource\Pages;
 
 use App\Filament\Resources\RentResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRent extends CreateRecord
 {
+
     protected static string $resource = RentResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['date_from'] = $data['date'][0];
+        $data['date_to'] = $data['date'][1];
+        return $data;
+    }
+
 }
